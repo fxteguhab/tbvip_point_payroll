c5a628c127431f5ec625d9fcf7fff2fc44661d4e
Devi Handevi <devihandevi@gmail.com>
2017-10-23 14:53:32 +0700

    [SALE] create draft point hook

--- sale.py
+++ sale.py
@@ -9,12 +9,14 @@ class sale_order(osv.osv):
 		result = super(sale_order, self).action_button_confirm(cr, uid, ids, context)
 		# input points
 		employee_point_obj = self.pool.get('hr.point.employee.point')
+		employee_obj = self.pool.get('hr.employee')
 		for sale in self.browse(cr, uid, ids):
 			value = sale.amount_total
 			row_count = len(sale.order_line)
 			qty_sum = 0
 			for line in sale.order_line:
 				qty_sum += line.product_uom_qty
+			# sales confirm (who confirm and employee responsible for the sales)
 			employee_point_obj.input_point(cr, uid,
 				activity_code='SALES',
 				roles={
@@ -27,6 +29,20 @@ class sale_order(osv.osv):
 					'BON_ROW_COUNT': row_count,
 				},
 				context=context)
+			# sales draft creator
+			employee_ids = employee_obj.search(cr, uid, [
+				('user_id', '=', sale.create_uid.id),
+			], limit=1, context=context)
+			if employee_ids and len(employee_ids) == 1:
+				employee_point_obj.input_point(cr, uid,
+					activity_code='ORDER',
+					roles={
+						'ADM': employee_ids[0],
+					},
+					required_parameters={
+						'BON_ROW_COUNT': row_count,
+					},
+					context=context)
 		return result
 	
 
