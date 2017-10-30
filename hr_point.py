c4dd5710507ad55265a5412f2dd35342d7102377
Devi Handevi <devihandevi@gmail.com>
2017-10-30 10:21:10 +0700

    [PAYROLL] rule

--- hr_point.py
+++ hr_point.py
@@ -8,10 +8,10 @@ class hr_point_type(osv.Model):
 	
 	def _create_salary_rule(self, cr, uid, point_type_name, context=None):
 		"""
-		Adds if inputs.SO_POINT > 0 else 0 clause for every created salary rules' python codes of hr_point_type for TBVIP
+		Adds if inputs.POIN_SALES_POINT.amount > 0 else 0 clause for every created salary rules' python codes of hr_point_type for TBVIP
 		"""
 		salary_rule_obj = self.pool.get('hr.salary.rule')
 		result = super(hr_point_type, self)._create_salary_rule(cr, uid, point_type_name, context)
 		salary = salary_rule_obj.browse(cr, uid, [result], context)
-		salary.amount_python_compute = salary.amount_python_compute + ' if inputs.SO_POINT > 0 else 0'
+		salary.amount_python_compute = salary.amount_python_compute + ' if inputs.POIN_SALES_POINT.amount > 0 else 0'
 		return result
