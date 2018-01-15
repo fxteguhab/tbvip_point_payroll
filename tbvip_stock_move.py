de2dc0baf62293e04af4a44998c2b17361421526
Devi Handevi <devihandevi@gmail.com>
2018-01-15 17:09:58 +0700

    [INTERBRANCH TRANSFER] bug fix

--- tbvip_stock_move.py
+++ tbvip_stock_move.py
@@ -7,35 +7,30 @@ from openerp.tools.translate import _
 class tbvip_interbranch_stock_move(osv.Model):
 	_inherit = 'tbvip.interbranch.stock.move'
 	
-	def create(self, cr, uid, vals, context=None):
-		result = super(tbvip_interbranch_stock_move, self).create(cr, uid, vals, context)
-		# input points
-		employee_point_obj = self.pool.get('hr.point.employee.point')
-		employee_obj = self.pool.get('hr.employee')
-		stock_location_obj = self.pool.get('stock.location')
-		employee_point_obj.input_point(cr, uid,
-			activity_code='INTERBRANCH_TRANSFER',
-			roles={
-				'ADM': [employee_obj.get_employee_id_from_user(cr, uid, vals['input_user_id'], context=context)],
-				'EMP': [vals['prepare_employee_id']],
-			},
-			required_parameters={
-				'ROW_COUNT': len(vals['interbranch_stock_move_line_ids']),
-			},
-			reference='Interbranch Transfer - From {} - To {}'.format(
-				stock_location_obj.browse(cr, uid, vals['from_stock_location_id']).name,
-				stock_location_obj.browse(cr, uid, vals['to_stock_location_id']).name),
-			context=context)
-		return result
-	
 	def action_accept(self, cr, uid, ids, context=None):
 		result = super(tbvip_interbranch_stock_move, self).action_accept(cr, uid, ids)
 		# input points
 		employee_point_obj = self.pool.get('hr.point.employee.point')
 		employee_obj = self.pool.get('hr.employee')
 		for sm in self.browse(cr, uid, ids):
+			# sender
 			employee_point_obj.input_point(cr, uid,
-				activity_code='INTERBRANCH_TRANSFER',
+				activity_code='INTERBRANCH_TRANSFER_SEND',
+				roles={
+					'ADM': [employee_obj.get_employee_id_from_user(cr, uid, sm.input_user_id.id, context=context)],
+					'EMP': [sm.prepare_employee_id.id],
+				},
+				required_parameters={
+					'ROW_COUNT': len(sm.interbranch_stock_move_line_ids.id),
+				},
+				reference='Interbranch Transfer - From {} - To {}'.format(
+					sm.from_stock_location_id.name,
+					sm.to_stock_location_id.name),
+				context=context)
+			
+			# receiver
+			employee_point_obj.input_point(cr, uid,
+				activity_code='INTERBRANCH_TRANSFER_RECEIVE',
 				roles={
 					'ADM': [employee_obj.get_employee_id_from_user(cr, uid, uid, context=context)],
 					'EMP': [sm.checked_by_id.id],
