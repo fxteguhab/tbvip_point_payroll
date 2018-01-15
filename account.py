d4084647a081f2e759d901b5a5c33ddee4b8fef8
Samuel Halimanto <sammyh27@gmail.com>
2018-01-15 12:42:59 +0700

    cron generate top point

--- account.py
+++ account.py
@@ -9,9 +9,7 @@ class tbvip_day_end(osv.osv):
 	def create(self, cr, uid, vals, context={}):
 		result_id = super(tbvip_day_end, self).create(cr, uid, vals, context)
 		
-		branch_obj = self.pool.get('tbvip.branch')
 		employee_obj = self.pool.get('hr.employee')
-		point_type_obj = self.pool.get('hr.point.type')
 		employee_point_obj = self.pool.get('hr.point.employee.point')
 		
 		day_end_date = vals['day_end_date']
@@ -54,55 +52,4 @@ class tbvip_day_end(osv.osv):
 			},
 			reference='Day End - {}'.format(day_end_date_datetime),
 			context=context)
-		
-		if vals['amend_number'] == 0:
-			# TOP
-			employee_points = {}
-			# Calculate today's points ( today = day_end_date )
-			for branch_id in branch_obj.search(cr, uid, [], context=context):
-				branch_employee_ids = employee_obj.get_employee_id_from_branch(cr, uid, branch_id, context=context)
-				total_branch_point = 0
-				for branch_employee_id in branch_employee_ids:
-					employee_points[branch_employee_id] = 0
-					# get employee points for today
-					today_employee_point_ids = employee_point_obj.search(cr, uid, [
-						('event_date', '>=', day_end_date_start),
-						('event_date', '<', day_end_date_end),
-						('employee_id', '=', branch_employee_id)
-					], context=context)
-					today_employee_points = employee_point_obj.browse(cr, uid, today_employee_point_ids, context=context)
-					for today_employee_point in today_employee_points:
-						employee_points[branch_employee_id] += today_employee_point.point # add to employee points
-						total_branch_point += today_employee_point.point # add to total
-				if total_branch_point > 0:
-					# if total branch point positive, calculate
-					for branch_employee_id in branch_employee_ids:
-						employee_points[branch_employee_id] = employee_points[branch_employee_id] / total_branch_point
-				else:
-					# if total branch point is zero or negative, no one gets anything
-					for branch_employee_id in branch_employee_ids:
-						employee_points[branch_employee_id] = 0
-						
-			# Search employee with the highest point from all branches
-			top_employee_id = 0
-			current_highest_point = 0
-			for employee_id in employee_points:
-				if employee_points[employee_id] > current_highest_point:
-					current_highest_point = employee_points[employee_id]
-					top_employee_id = employee_id
-			
-			# Input point TOP today if highest point of employee is not zero
-			if current_highest_point > 0:
-				point_type_ids = point_type_obj.search(cr, uid, [
-					('name', '=', 'POIN_TOP')
-				], limit=1, context=context)
-				if point_type_ids and len(point_type_ids) == 1:
-					employee_point_vals = {
-						'event_date': day_end_date_datetime,
-						'employee_id': top_employee_id,
-						'point_type_id': point_type_ids[0],
-						'point': 1,
-						'reference': 'Day End - {}'.format(day_end_date_datetime),
-					}
-					new_employee_point_id = employee_point_obj.create(cr, uid, employee_point_vals, context)
 		return result_id
