26e077deaefcef190801536e3bc8f67e576c210f
Devi Handevi <devihandevi@gmail.com>
2018-01-15 14:55:08 +0700

    [POINT DATA] price list, current product price buy

--- price_list.py
+++ price_list.py
@@ -13,13 +13,21 @@ class price_list(osv.osv):
 		# input points
 		employee_point_obj = self.pool.get('hr.point.employee.point')
 		employee_obj = self.pool.get('hr.employee')
+		price_type_obj = self.pool.get('price.type')
+		# get activity code
+		activity_code = 'SALES_PRICE_LIST'
+		price_type = price_type_obj.browse(cr, uid, vals['price_type_id'], context=context)
+		if price_type.type == 'sell':
+			activity_code = 'PURCHASE_PRICE_LIST'
+		# get row count
 		row_count = 0
 		if vals['type'] == 'product':
 			row_count += len(vals['line_product_ids'])
 		elif vals['type'] == 'category':
 			row_count += len(vals['line_category_ids'])
+		# input
 		employee_point_obj.input_point(cr, uid,
-			activity_code='PRICE_LIST',
+			activity_code=activity_code,
 			roles={
 				'ADM': [employee_obj.get_employee_id_from_user(cr, uid, uid, context=context)],
 			},
@@ -41,8 +49,14 @@ class product_current_price(osv.osv):
 		employee_point_obj = self.pool.get('hr.point.employee.point')
 		employee_obj = self.pool.get('hr.employee')
 		product_obj = self.pool.get('product.product')
+		price_type_obj = self.pool.get('price.type')
+		# get activity code
+		activity_code = 'SALES_PRODUCT_CURRENT_PRICE'
+		price_type = price_type_obj.browse(cr, uid, vals['price_type_id'], context=context)
+		if price_type.type == 'sell':
+			activity_code = 'PURCHASE_PRODUCT_CURRENT_PRICE'
 		employee_point_obj.input_point(cr, uid,
-			activity_code='PRODUCT_CURRENT_PRICE',
+			activity_code=activity_code,
 			roles={
 				'ADM': [employee_obj.get_employee_id_from_user(cr, uid, uid, context=context)],
 			},
