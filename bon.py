e04dce120441b5294e67955e58ea639a493d2b03
fxteguhab <31503256+fxteguhab@users.noreply.github.com>
2018-09-18 08:20:52 +0700

    pindahin & benerin cek_last_book

--- bon.py
+++ bon.py
@@ -7,17 +7,9 @@ from openerp.tools.translate import _
 class tbvip_bon_book(osv.osv):
 	_inherit = "tbvip.bon.book"
 	
-	def _cek_last_book_residual(self,cr,uid,employee_id):
-		bon_book_ids = self.search(cr, uid,[('employee_id','=',employee_id)],order = "id desc")
-		last_id = bon_book_ids[1]
-		bon = self.browse(cr, uid, last_id)
-		residual = (bon.end_at - bon.start_from + 1)-bon.total_used
-		return residual
-
-
 	def create(self, cr, uid, vals, context=None):
 		result = super(tbvip_bon_book, self).create(cr, uid, vals, context)
-		residual = 	self._cek_last_book_residual(cr,uid,vals['employee_id'])
+		residual = 	self._cek_last_book_residual(cr,uid,vals['employee_id'],vals['branch_id'])
 		# input points
 		employee_point_obj = self.pool.get('hr.point.employee.point')
 		employee_obj = self.pool.get('hr.employee')
